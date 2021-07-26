5cfbdc6aede3417c0f5b28f81da095c60ba929e7
Ykn774 <oparag96@gmail.com>
2021-07-26 21:33:50 +0545

    created custom widgets for browser QPushButtons and QThread

--- nhentaiExplorer/Search.py
+++ nhentaiExplorer/Search.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 
 from nhentaiExplorer.CustomWidgets import SearchBoxWidget
+from nhentaiExplorer.CustomWidgets import QPushButton
 
 
 class Search(SearchBoxWidget):
