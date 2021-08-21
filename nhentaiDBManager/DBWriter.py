e0d719e3e9a1a3ed8a58f0ddb69b2d34676d1150
Ykn774 <oparag96@gmail.com>
2021-08-21 12:37:37 -0400

    changed file writing from chunks to whole file at once

--- nhentaiDBManager/DBWriter.py
+++ nhentaiDBManager/DBWriter.py
@@ -3,7 +3,6 @@ import sqlite3
 import csv
 import re
 import sys
-
 class nhentaiLibrary:
     def create_database(self) -> None:
         self.c.execute(""" CREATE TABLE IF NOT EXISTS nhentaiLibrary (
